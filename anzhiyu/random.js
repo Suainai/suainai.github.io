067b2eab131b1f41ba3e84ab3e1002fbdd25ca54
Suainai <3278352909@qq.com>
2025-12-29 18:16:18 +0800

    Site updated: 2025-12-29 18:16:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/04/Angular/","2025/05/27/hello-world/","2025/06/04/Spring/"];function toRandomPost(){
+var posts=["2025/05/27/hello-world/","2025/06/04/Angular/","2025/06/04/Spring/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
